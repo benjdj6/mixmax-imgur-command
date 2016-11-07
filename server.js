1cc791bf2e2ca1050d5318972563f5a4317226a4
benjdj6 <benjdj6@vt.edu>
2016-11-07 17:23:01 -0500

    Added HTTPS to make chrome happy

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ var bodyParser = require('body-parser');
 var app = express();
 var sync = require('synchronize');
 var cors = require('cors');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 
+var key = fs.readFileSync('sslcert/server.key', 'utf8');
+var cert = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+var cred = {key: key, cert: cert};
 
 app.use(function(req, res, next) {
   sync.fiber(next);
@@ -18,4 +25,8 @@ var corsOptions = {
 app.get('/typeahead', cors(corsOptions), require('./routes/typeahead'));
 app.get('/resolver', cors(corsOptions), require('./routes/resolver'));
 
-app.listen(process.env.PORT || 8080);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(cred, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
